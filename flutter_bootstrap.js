5d77aee8f7d31be02e50e03e36107958341da424
Eddie <walkmandede@gmail.com>
2026-05-01 16:17:28 +0700

    deploy 1.2.0prod

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3602740146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "80668688" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
